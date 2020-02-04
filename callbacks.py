8f4c537f4b310cee32bde91a0da75ae037ee770b
Zach Howard <obdo32@gmail.com>
2020-02-04 15:16:00 -0800

    less popups

--- callbacks.py
+++ callbacks.py
@@ -1,26 +1,7 @@
-import random
-import string
-
 import cherrypy
 
 # CONFIG = {'/callback':{'server.socket_host': '127.0.0.1','server.socket_port': 9000,'log.screen': False}}
 cherrypy.config.update({"log.screen": False})
-
-
-class CallbackHandler(object):
-    response = None
-
-    @cherrypy.expose
-    def index(self):
-        request = cherrypy.serving.request
-        self.response = cherrypy.url(qs=request.query_string)
-        cherrypy.engine.exit()
-
-
-def await_callback():
-    handle = CallbackHandler()
-    cherrypy.quickstart(handle, "/callback", config={"/": {"log.screen": False}})
-    return handle.response
 
 
 def wait_for_http_callback(_port=9000, _host="127.0.0.1"):
